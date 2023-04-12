32067661b88ab09a748bdf9540a08a1dfbad197d
Ghosteye <yqcyecong@163.com>
2023-04-12 17:36:01 +0800

    chore: Update initapi

--- api.config.ts
+++ api.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'initapi'
 export default defineConfig({
   importAxios: 'import { request } from "../index";',
   useAxios: 'request',
-  swagger: {
+  service: {
     test: 'https://petstore.swagger.io/v2/swagger.json',
     mangosteen: 'http://127.0.0.1:3000/api-json',
   },
